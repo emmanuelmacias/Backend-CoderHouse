5536d228f510cfab2f1905e93b34bbf4e6a7aef8
emma.macias <emmamaciasdf@gmail.com>
2023-04-11 20:53:59 -0300

    Se agrego CODE y comprobacion del CODE

--- desafio.js
+++ desafio.js
@@ -9,22 +9,32 @@ class ProductManager{
         return this.products;
     }
 
-    addProduct(title, description, price, thumbnail, stock) {
-        const product = {
-            id: ++this.idCounter,
-            title,
-            description,
-            price,
-            thumbnail,
-            stock,
-        };
-        return this.products.push(product);
-    }
+    addProduct(title, description, price, thumbnail, code, stock) {
 
+        const existingCode = this.products.find(product => product.code === code); 
+
+        if(existingCode){
+            console.log(`Ya existe un producto con el código ${code}`);
+        } else {
+
+            const product = {
+                id: ++this.idCounter,
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock,
+            };
+            
+            return this.products.push(product);
+        }
+    }
+    
     #existingProduct(idProduct) {
         return this.products.find(products => products.id === idProduct);
     }
-    
+
     getProductById(idProduct) {
         const product = this.#existingProduct(idProduct);
         if (!product){
@@ -41,12 +51,16 @@ const productManager = new ProductManager();
 console.log(productManager.getProducts());
 
 // Se agrega un producto
-productManager.addProduct('produto prueba', 'Este es un producto prueba', 200, 'Sin Imagen', 25);
+productManager.addProduct('produto prueba', 'Este es un producto prueba', 200, 'Sin Imagen', 'ABC123', 25);
 
-// El producto se incluye
+// Se muestran los productos 
+console.log(productManager.getProducts());
+
+// Se agrega un nuevo producto , si se agrega un producto con el mismo CODE muestra un error
+productManager.addProduct('produto prueba 2', 'Este es un producto prueba', 250, 'Sin Imagen', 'ABC124', 25);
+
+// Se muestran los productos 
 console.log(productManager.getProducts());
 
 // Se obtiene producto por ID
-console.log(productManager.getProductById(1)); // Si se prueba con otro ID , se obtiene un error
-
-
+console.log(productManager.getProductById(2)); // Si se prueba con otro ID que no este listado , se obtiene un error
