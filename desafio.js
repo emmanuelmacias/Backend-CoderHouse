d2f8eaf41930ec47a8ec8f59872d730ca33b6c98
emma.macias <emmamaciasdf@gmail.com>
2023-04-16 16:17:14 -0300

    Manejo de Archivos FS

--- desafio.js
+++ desafio.js
@@ -1,66 +1,152 @@
-class ProductManager{
-    
-    constructor(){
-        this.products = [];
-        this.idCounter = 0;
+const fs = require('fs');
+
+class ProductManager {
+  constructor() {
+    this.idCounter = 0;
+    this.path = "./products.json";
+  }
+
+  async getProducts() {
+    try {
+      if (fs.existsSync(this.path)) {
+        const products = await fs.promises.readFile(this.path, 'utf8');
+        const productsJS = JSON.parse(products);
+        return productsJS;
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    getProducts() {
-        return this.products;
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    try {
+      const productsFile = await this.getProducts();
+      const existingCode = productsFile.find(
+        (product) => product.code === code
+      );
+
+      if (existingCode) {
+        console.log(`Ya existe un producto con el código ${code}`);
+      } else {
+        const lastProduct = productsFile[productsFile.length - 1];
+        const newId = lastProduct ? lastProduct.id + 1 : 1;
+        const product = {
+          id: newId,
+          title,
+          description,
+          price,
+          thumbnail,
+          code,
+          stock,
+        };
+
+        productsFile.push(product);
+        await fs.promises.writeFile(this.path, JSON.stringify(productsFile));
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
-
-        const existingCode = this.products.find(product => product.code === code); 
-
-        if(existingCode){
-            console.log(`Ya existe un producto con el código ${code}`);
-        } else {
-
-            const product = {
-                id: ++this.idCounter,
-                title,
-                description,
-                price,
-                thumbnail,
-                code,
-                stock,
-            };
-            
-            return this.products.push(product);
-        }
+  async deleteProduct(idProduct) {
+    try {
+      const productsFile = await this.getProducts();
+      const productIndex = productsFile.findIndex((product) => product.id === idProduct);
+  
+      if (productIndex === -1) {
+        console.log('No se encontró el producto con el ID especificado');
+      } else {
+        productsFile.splice(productIndex, 1);
+        await fs.promises.writeFile(this.path, JSON.stringify(productsFile));
+        console.log(`Producto con ID ${idProduct} eliminado exitosamente`);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    
-    #existingProduct(idProduct) {
-        return this.products.find(products => products.id === idProduct);
-    }
+  }
 
-    getProductById(idProduct) {
-        const product = this.#existingProduct(idProduct);
-        if (!product){
-            console.log('Not found');
-        } 
-        return product; 
+  async updateProduct(idProduct, updatedFields) {
+    try {
+      const productsFile = await this.getProducts();
+      const productIndex = productsFile.findIndex((product) => product.id === idProduct);
+  
+      if (productIndex === -1) {
+        console.log('No se encontró el producto con el ID especificado');
+      } else {
+        const updatedProduct = {
+          ...productsFile[productIndex],
+          ...updatedFields,
+          id: idProduct, // aseguramos que no se cambie el ID original
+        };
+        productsFile[productIndex] = updatedProduct;
+        await fs.promises.writeFile(this.path, JSON.stringify(productsFile));
+        console.log(`Producto actualizado exitosamente`);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  async #existingProduct(idProduct) {
+    try {
+      const productsFile = await this.getProducts();
+      return productsFile.find((products) => products.id === idProduct);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getProductById(idProduct) {
+    try {
+      const product = this.#existingProduct(idProduct);
+      if (!product) {
+        console.log('Not found');
+      } else {
+        return product;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 // Se crea la instancia de ProductManager
 const productManager = new ProductManager();
 
 // Se obtienen los productos devuelve un Array vacío
-console.log(productManager.getProducts());
+const test = async () => {
+  const get1 = await productManager.getProducts();
+  console.log("Consulta inicial: ", get1);
+  // Se agrega un producto (si tiene el mismo Code arroja error y no se agrega al file)
+  await productManager.addProduct(
+    "produto prueba",
+    "Este es un producto prueba",
+    200,
+    "Sin Imagen",
+    "ABC123",
+    25
+  );
+  // Consulta  de producto por ID
+  const get2 = await productManager.getProductById(2);
+  console.log("Consulta por ID: ", get2);
+  // Consulta del Array de productos
+  const get3 = await productManager.getProducts();
+  console.log("Segunda consulta: ", get3);
+  //Eliminar producto por ID
+  await productManager.deleteProduct(1);
+  const get4 = await productManager.getProducts();
+  console.log("Tercer Consulta: ", get4);
+  // Actualizar producto por ID (Precio y Stock)
+  await productManager.updateProduct(2, { 
+    price: 300,
+    stock: 30,
+  });
+  // Consulta Final
+  const get5 = await productManager.getProducts();
+  console.log("Tercer Consulta: ", get5);
 
-// Se agrega un producto
-productManager.addProduct('produto prueba', 'Este es un producto prueba', 200, 'Sin Imagen', 'ABC123', 25);
+};
 
-// Se muestran los productos 
-console.log(productManager.getProducts());
-
-// Se agrega un nuevo producto , si se agrega un producto con el mismo CODE muestra un error
-productManager.addProduct('produto prueba 2', 'Este es un producto prueba', 250, 'Sin Imagen', 'ABC124', 25);
-
-// Se muestran los productos 
-console.log(productManager.getProducts());
-
-// Se obtiene producto por ID
-console.log(productManager.getProductById(2)); // Si se prueba con otro ID que no este listado , se obtiene un error
+test();
